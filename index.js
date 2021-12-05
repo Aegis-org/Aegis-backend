e33ec285128a716577e5bd9f7508cd4314086d88
Williams Samuel <samwill300@gmail.com>
2021-12-05 22:47:13 +0100

    local db connected, mark #7 as done

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+const url = 'mongodb://127.0.0.1:27017/aegis';
 
+const connectDB = async () => {
+	try {
+		await mongoose.connect(url, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log(`MongoDB Connected: ${url}`);
+	} catch (err) {
+		console.error(err);
+	}
+};
 
+connectDB();
 
-
-
-
-app.listen(8080,()=>{
-    console.log('Backend App running on port 8080 Aieges')
-})
-
+app.listen(8080, () => {
+	console.log('Backend App running on port 8080 Aieges');
+});

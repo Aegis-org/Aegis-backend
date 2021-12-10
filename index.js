39e084487ecaaefac66bea62913f5a6ff008d56c
Williams Samuel <samwill300@gmail.com>
2021-12-10 21:45:40 +0100

    finished signup authentication, close #11

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-const   express             =       require('express'),
-        mongoose            =       require('mongoose'),
-        passport            =       require('passport'),
-        path                =       require('path');
+const express = require('express'),
+	bodyparser = require('body-parser'),
+	cookieparser = require('cookie-parser'),
+	User = require('./models/UserDetails'),
+	connectDB = require('./db/connect');
 
 const app = express();
 
-const url = 'mongodb://127.0.0.1:27017/aegis';
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cookieparser());
 
-const connectDB = async () => {
-	try {
-		await mongoose.connect(url, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log(`MongoDB Connected: ${url}`);
-	} catch (err) {
-		console.error(err);
-	}
-};
+app.get('/', async (req, res) => {
+	res.sendFile(path.join(__dirname, 'aigis/index.html'));
+});
 
-connectDB();
+// ======= USER SIGN UP ========
+app.post('/api/user/signup', (req, res) => {
+	const newUser = new User(req.body);
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'aigis/index.html'))
-})
+	User.find(
+		{ email: newUser.email, username: newUser.username },
+		function (err, user) {
+			if (user.email) {
+				return res
+					.status(400)
+					.json({ auth: false, message: 'email already exists' });
+			} else if (user.username) {
+				return res
+					.status(400)
+					.json({ auth: false, message: 'username already exists' });
+			}
+
+			newUser.save((err, doc) => {
+				if (err) {
+					console.log(err);
+					return res.status(400).json({ success: false });
+				}
+				res.status(200).json({
+					succes: true,
+					user: doc,
+				});
+			});
+		}
+	);
+});
 
 app.listen(8080, () => {
 	console.log('Backend App running on port 8080 Aieges');
 });
+
+connectDB();

971797eac5a78cf01baf7ac285c40c8588b7695f
0tuedon <tuedonmofe@gmail.com>
2021-12-14 00:46:13 +0100

    merge conflict resolved

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
+
+require('dotenv').config();
 const express = require('express'),
 	bodyparser = require('body-parser'),
 	cookieparser = require('cookie-parser'),
 	User = require('./models/UserDetails'),
 	connectDB = require('./db/connect'),
-	apiRoute = require('./routes')
+	apiRoute = require('./routes'),
+	config = require('./config/axios-config');
 
 const app = express();
 const mongoose = require('mongoose');
-const path = require('path')
-app.set('view engine', 'ejs')
+const path = require('path');
+app.set('view engine', 'ejs');
 
 //passport
-const passport = require('passport')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const LocalStrategy = require('passport-local')
-const connectEnsureLogin = require('connect-ensure-login')
 // RUNS THE CONNECTION TO THE MONGODB SERVER 
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const LocalStrategy = require('passport-local');
+const connectEnsureLogin = require('connect-ensure-login');
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'))
@@ -24,13 +28,19 @@ app.use(bodyparser.json());
 app.use(cookieparser());
 connectDB();
 // ======= USER SIGN UP ========
+
 app.get('/', (req, res) => {
 	res.render('index')
 })
 app.use(apiRoute)
 
-app.listen(8080, () => {
-	console.log('Backend App running on port 8080 Aieges');
+app.use(apiRoute);
+
+app.get('/', (req, res) => {
+	res.render(path.join(__dirname, 'aigis/index'));
 });
 
 
+app.listen(8080, () => {
+	console.log('Backend App running on port 8080 Aieges');
+});
